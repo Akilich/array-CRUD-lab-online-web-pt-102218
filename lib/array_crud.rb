505701067b8ee261f0f1a57ecef2ca7b95330d1b
Akili Humphrey <akilihumphrey@gmail.com>
2018-10-27 04:09:34 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,12 +19,12 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array.pop("arrays!")
+  array("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array("arrays!")
+  array.shift("arrays!")
 end
 
 def retrieve_element_from_index(array, index_number)
