5cfde57bcab949eb632eb81c3ced6efeb6ed1bd8
Akili Humphrey <akilihumphrey@gmail.com>
2018-10-27 03:57:32 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   names = ["Akili", "Kristie", "Kira", "Charles"]
-  puts names.push("Christina")
+  names.push("Christina")
 end
 
 def add_element_to_start_of_array(array, element)
